a2cf89d41fecbc4cd2d6519885e161a8f8ef5022
Takuya Nishimoto <nishimotz@gmail.com>
2010-12-28 23:10:23 +0900

    silence driver works

--- source/synthDrivers/silence.py
+++ source/synthDrivers/silence.py
@@ -4,14 +4,28 @@
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
 
+from collections import OrderedDict
 import synthDriverHandler
+from synthDriverHandler import SynthDriver, VoiceInfo
 
 class SynthDriver(synthDriverHandler.SynthDriver):
 	"""A dummy synth driver used to disable speech in NVDA.
 	"""
+	supportedSettings=(SynthDriver.VoiceSetting(),)
 	name="silence"
 	description=_("No speech")
 
 	@classmethod
 	def check(cls):
 		return True
+
+	def _get_voice(self):
+		return "S1"
+
+	def _set_voice(self,value):
+		pass
+
+	def _getAvailableVoices(self):
+		voices=OrderedDict()
+		voices['S1']=VoiceInfo('S1','silence','ja_JP')
+		return voices
